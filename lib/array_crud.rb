6eee62a97b8213cc5f56e764f4dd6cda6068d95f
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-06 22:22:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ def create_an_empty_array
 end
 
 def create_an_array
- friends=[Ron,Harry,Hermione,Luna]
+ friends=["Ron", "Harry", "Hermione", "Luna"]
  friends
 end
 
@@ -25,7 +25,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-array.index[index_number]  
+array.[index_number]  
 end
 
 def retrieve_first_element_from_array(array)
