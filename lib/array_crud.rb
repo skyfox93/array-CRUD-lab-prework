e95bb838359141f55c3e286f6323b1e97b670021
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-06 22:13:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
 def create_an_empty_array
-  
+  friends=[]
+  friends
 end
 
 def create_an_array
-  
+ friends=[Ron,Harry,Hermione,Luna]
+ friends
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
